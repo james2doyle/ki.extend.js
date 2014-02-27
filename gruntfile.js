2208ccf83a1bd7f4f243183918b99153ebefed04
james2doyle <james2doyle@gmail.com>
2014-02-27 16:37:22 -0500

    add gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*!\n * <%= pkg.name %> - <%= pkg.description %>\n' +
+      ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>)\n' +
+      ' * Released under <%= _.pluck(pkg.licenses, "type").join(", ") %> license\n */\n'
+    },
+    concat: {
+      options: {
+        separator: '\n',
+      },
+      dist: {
+        src: [
+        'build/parts/header.js',
+        'build/parts/classes.js',
+        'build/parts/append-prepend.js',
+        'build/parts/attr.js',
+        'build/parts/before-after.js',
+        'build/parts/css.js',
+        'build/parts/first-last-get.js',
+        'build/parts/html-text.js',
+        'build/parts/parent.js',
+        'build/parts/remove.js',
+        'build/parts/trim.js',
+        'build/parts/trigger.js',
+        'build/parts/is.js',
+        'build/parts/map.js',
+        'build/parts/stop.js',
+        'build/parts/ajax.js',
+        'build/parts/footer.js'
+        ],
+        dest: 'ki.extend.js',
+      },
+    },
+    uglify: {
+      normal: {
+        files: {
+          'ki.extend.min.js': ['ki.extend.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
